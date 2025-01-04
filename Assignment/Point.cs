34836e0e8ec0829034b23b84a654902fa727d48d
Sajeda-Mohamed <sagedamohameed55@gmail.com>
2025-01-04 17:03:31 +0200

    Q01-Calc the distance

---
+++ Assignment/Point.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal struct Point
+    {
+        #region Properties
+        public double X { get; set; }
+        public double Y { get; set; }
+        #endregion
+
+        #region Costructors
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+        #endregion
+
+        #region Methods
+        public static double CalcDistance(Point P01, Point P02)
+        {
+            return Math.Sqrt(Math.Pow(P01.X - P02.X,2) + Math.Pow(P01.Y - P02.Y,2));
+        }
+        #endregion
+    }
+}
---
+++ Assignment/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q01-Calc the distance
+            //Console.WriteLine("Enter X of the Point01 :");
+            //bool flag01 = double.TryParse(Console.ReadLine(), out double x01);
+            //Console.WriteLine("Enter Y of the Point01 :");
+            //bool flag02 = double.TryParse(Console.ReadLine(), out double y01);
+
+            //Console.WriteLine("Enter X of the Point02 :");
+            //bool flag03 = double.TryParse(Console.ReadLine(), out double x02);
+            //Console.WriteLine("Enter Y of the Point02 :");
+            //bool flag04 = double.TryParse(Console.ReadLine(), out double y02);
+
+            //Point P01 = new Point(x01,y01);
+            //Point P02 = new Point(x02,y02);
+
+            //Console.WriteLine("The Distance between 2 Point => "+Point.CalcDistance(P01, P02));
+            #endregion
+        }
+    }
+}
