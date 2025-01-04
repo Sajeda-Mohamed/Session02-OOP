9bae20e4672dd30d0cac95f568072a8f001f7d91
Sajeda-Mohamed <sagedamohameed55@gmail.com>
2025-01-04 17:34:38 +0200

    Q02-Detials Oldest Person

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -20,6 +20,28 @@
 
             //Console.WriteLine("The Distance between 2 Point => "+Point.CalcDistance(P01, P02));
             #endregion
+
+            #region Q02-Detials Oldest Person
+            //Console.WriteLine("Enter the Name of Person 01 : ");
+            //string Name01 = Console.ReadLine();
+            //Console.WriteLine("Enter the Age of Person 01 : ");
+            //bool flag01 = int.TryParse(Console.ReadLine(), out int Age01);
+            //Person person01 = new Person(Age01, Name01);
+
+            //Console.WriteLine("Enter the Name of Person 02 : ");
+            //string Name02 = Console.ReadLine();
+            //Console.WriteLine("Enter the Age Person 02: ");
+            //bool flag02 = int.TryParse(Console.ReadLine(), out int Age02);
+            //Person person02 = new Person(Age02, Name02);
+
+            //Console.WriteLine("Enter the Name Person 03: ");
+            //string Name03 = Console.ReadLine();
+            //Console.WriteLine("Enter the Age Person 03: ");
+            //bool flag03 = int.TryParse(Console.ReadLine(), out int Age03);
+            //Person person03 = new Person(Age03, Name01);
+
+            //Person.CheckOldestPerson(person01, person02, person03);
+            #endregion
         }
     }
 }
